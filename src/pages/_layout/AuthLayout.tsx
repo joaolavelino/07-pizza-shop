4dc6c8f0f8a99343fb7fa7b65097da866d27a513
João Avelino <joaolavelino@gmail.com>
2025-07-02 17:01:19 -0300

    login-form with RHF

--- src/pages/_layout/AuthLayout.tsx
+++ src/pages/_layout/AuthLayout.tsx
@@ -1,22 +1,39 @@
+import { Button } from '@/components/ui/button'
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export interface AuthLayoutProps {}
 
 export const AuthLayout: React.FC<AuthLayoutProps> = () => {
   return (
-    <>
-      <header>
-        <h1>Header of the authorization pages</h1>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-      <footer>
-        <p>Created during the React learning path from Rocketseat. </p>
-        <a href="https://github.com/joaolavelino/06-ingite-github-blog/issues">
-          Check here all the learning notes on Github
-        </a>
-      </footer>
-    </>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="border-r-2-foreground text-muted-foreground flex h-full flex-col justify-between bg-[url(pizza-dark.jpg)] bg-cover bg-center">
+        <div className="text-foreground flex items-center gap-3 p-8 text-4xl font-thin">
+          <h1 className="flex gap-4">
+            <Pizza strokeWidth={1} size={40} /> pizza.shop
+          </h1>
+        </div>
+        <footer className="p-8 text-sm">
+          <p>
+            Painel do Parceiro &copy; pizza.shop - {new Date().getFullYear()}
+          </p>
+          <p>Created during the React learning path from Rocketseat. </p>
+          <Button asChild variant="outline">
+            <a
+              className="mt-2 italic"
+              href="https://github.com/joaolavelino/06-ingite-github-blog/issues"
+            >
+              Check here all the learning notes on Github
+            </a>
+          </Button>
+        </footer>
+      </div>
+      <div className="flex h-full flex-col">
+        <main className="flex flex-1 flex-col items-center justify-center">
+          <Outlet />
+        </main>
+        <footer></footer>
+      </div>
+    </div>
   )
 }
--- src/pages/auth/SignInPage.tsx
+++ src/pages/auth/SignInPage.tsx
@@ -1,12 +1,59 @@
 import type { DefaultPageProps } from '@/_types/pagesTypes'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { HourglassIcon } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 export interface SignInPageProps extends DefaultPageProps {}
 
+const signInSchema = z.object({
+  email: z.string().email(),
+})
+
+type signInFormType = z.infer<typeof signInSchema>
+
 export const SignInPage: React.FC<SignInPageProps> = ({ title }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<signInFormType>()
+
+  async function handleSignIn(data: signInFormType) {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    console.log(data)
+  }
   return (
     <>
       <title>{title}</title>
-      <h1>Sign-in Page</h1>
+      <div className="bg-muted rounded-sm border-1 p-6">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-2xl font-thin tracking-tight">
+              Access dashboard
+            </h1>
+            <p className="text-muted-foreground text-sm font-light">
+              Track your sales through the partner dashboard
+            </p>
+          </div>
+
+          <form
+            action=""
+            className="space-y-4"
+            onSubmit={handleSubmit(handleSignIn)}
+          >
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+            <Button className="w-full" type="submit" disabled={isSubmitting}>
+              {isSubmitting ? <HourglassIcon /> : 'Log-in'}
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
