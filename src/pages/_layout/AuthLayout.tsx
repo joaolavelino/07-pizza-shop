a2a627af5f35f7c904ee8a1d01ff1efae72dde08
João Avelino <joaolavelino@gmail.com>
2025-07-03 11:26:22 -0300

    Auth layout responsive layout

--- src/pages/_layout/AuthLayout.tsx
+++ src/pages/_layout/AuthLayout.tsx
@@ -6,19 +6,23 @@ export interface AuthLayoutProps {}
 
 export const AuthLayout: React.FC<AuthLayoutProps> = () => {
   return (
-    <div className="grid min-h-screen grid-cols-2">
+    <div className="grid min-h-screen lg:grid-cols-2">
       <div className="border-r-2-foreground text-muted-foreground flex h-full flex-col justify-between bg-[url(pizza-dark.jpg)] bg-cover bg-center">
-        <div className="text-foreground flex items-center gap-3 p-8 text-4xl font-thin">
-          <h1 className="flex gap-4">
-            <Pizza strokeWidth={1} size={40} /> pizza.shop
-          </h1>
+        <h1 className="text-foreground flex items-center gap-3 p-8 text-4xl font-thin">
+          <Pizza strokeWidth={1} size={40} /> pizza.shop
+        </h1>
+        <div className="flex h-full flex-col lg:hidden">
+          <main className="flex flex-1 flex-col items-center justify-center">
+            <Outlet />
+          </main>
+          <footer></footer>
         </div>
         <footer className="p-8 text-sm">
           <p>
             Painel do Parceiro &copy; pizza.shop - {new Date().getFullYear()}
           </p>
           <p>Created during the React learning path from Rocketseat. </p>
-          <Button asChild variant="outline">
+          <Button asChild variant="secondary">
             <a
               className="mt-2 italic"
               href="https://github.com/joaolavelino/06-ingite-github-blog/issues"
@@ -28,7 +32,7 @@ export const AuthLayout: React.FC<AuthLayoutProps> = () => {
           </Button>
         </footer>
       </div>
-      <div className="flex h-full flex-col">
+      <div className="hidden h-full flex-col lg:flex">
         <main className="flex flex-1 flex-col items-center justify-center">
           <Outlet />
         </main>
