bed1e566a2118d676570d5c6528c93149b0f28ed
João Avelino <joaolavelino@gmail.com>
2025-07-07 15:35:59 -0300

    styled footer

--- src/pages/_layout/AppLayout.tsx
+++ src/pages/_layout/AppLayout.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@/components/Header'
+import { Button } from '@/components/ui/button'
 import { Outlet } from 'react-router-dom'
 
 export interface AppLayoutProps {}
@@ -10,11 +11,17 @@ export const AppLayout: React.FC<AppLayoutProps> = () => {
       <main className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </main>
-      <footer>
+      <footer className="text-muted-foreground flex w-screen flex-col items-center text-sm">
         <p>Created during the React learning path from Rocketseat. </p>
-        <a href="https://github.com/joaolavelino/06-ingite-github-blog/issues">
-          Check here all the learning notes on Github
-        </a>
+        <Button variant={'link'}>
+          <a
+            href="https://github.com/joaolavelino/06-ingite-github-blog/issues"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Check here all the learning notes on Github
+          </a>
+        </Button>
       </footer>
     </div>
   )
