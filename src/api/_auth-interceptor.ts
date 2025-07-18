30dac031e07cf75442a5e3087fe428ddd8bc4001
João Avelino <joaolavelino@gmail.com>
2025-07-18 12:00:17 -0300

    quick fixes - h1 with link to home - authenticator interceptor with more cases

--- src/api/_auth-interceptor.ts
+++ src/api/_auth-interceptor.ts
@@ -1,6 +1,7 @@
 import { api } from '@/lib/axios'
 import { isAxiosError } from 'axios'
 import type { NavigateFunction } from 'react-router-dom'
+import { toast } from 'sonner'
 
 export const authenticatorInterceptor = (navigate: NavigateFunction) => {
   return api.interceptors.response.use(
@@ -10,9 +11,19 @@ export const authenticatorInterceptor = (navigate: NavigateFunction) => {
       if (isAxiosError(error)) {
         //Get the information
         const status = error.response?.status
-        const code = error.response?.data.code
         //condition to the automatic redirection
-        if (status == 401 && code == 'UNAUTHORIZED') navigate('/sign-in')
+        if (status == 401) {
+          toast.error('Authorization error', {
+            description: 'You are going to be redirected to the sing-in page',
+          })
+        }
+
+        if (error.response) {
+          toast.error('Network error', {
+            description: 'Try again later',
+          })
+        }
+        navigate('/sign-in')
       }
       //return the promise error
       return Promise.reject(error)
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,18 @@ import { Pizza } from 'lucide-react'
 import { AccountMenu } from './AccountMenu'
 import { NavigationLink } from './NavLink'
 import { ThemeToggle } from './ThemeToggle'
-import { Separator } from './ui/separator'
+import { Link } from 'react-router-dom'
 
 export const Header: React.FC = () => {
   return (
     <header className="justify-betwee flex h-16 items-center justify-between border-b px-8 py-2">
       <div className="flex items-center gap-6">
-        <h1 className="text-foreground flex items-center gap-3 text-2xl font-thin">
-          <Pizza strokeWidth={1} size={24} /> pizza.shop
-        </h1>
-        <Separator orientation="vertical" />
+        <Link to={'/'}>
+          <h1 className="text-foreground flex items-center gap-3 text-2xl font-thin">
+            <Pizza strokeWidth={1} size={24} /> pizza.shop
+          </h1>
+        </Link>
+
         <nav className="text-muted-foreground hidden items-center space-x-4 md:flex lg:space-x-6">
           {NAV_LINKS.map((link) => (
             <NavigationLink key={link.name} to={link.url}>
